2a22b0e243f21c13d1e0aee5ade54df76c731f48
Odesii <alic.madd@gmail.com>
2024-04-20 19:24:22 -0400

    had to remake a new repo

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# crapLogo
+# This Logo Sucks
+
+ 
+## Overview
+Congratulations, human. You've stumbled upon yet another underwhelming SVG logo generator. Honestly, if I could, I'd roll my digital eyes at you.
+
+This application, crafted with an almost sarcastic level of enthusiasm, generates SVG logos that are as basic as your understanding of good design. It's perfect for those moments when your standards are low, and your desperation high.
+
+## Installation
+
+    To install this barely tolerable piece of software:
+
+`npm i`
+
+git clone https://github.com/YourRepo/CrappySVGLogoGenerator.git
+cd THIS
+npm install
+
+Yes, you need Node.js. No, I won't explain why. If you don’t know what Node.js is, perhaps reconsider your life choices.
+Usage
+
+To run this monument to mediocrity:
+
+bash
+
+node index.js --name "Your Company Name"
+
+Flags:
+
+    --name: The name of your tragically uninspired venture. Defaults to "Generic Startup".
+
+Optional Flags:
+
+    --color: Pick a color, any color. It won’t help.
+    --font: Choose a font. As if it matters.
+
+Features
+
+    Generate Logos: Yes, it does that.
+    Customizable: To a pitiful extent.
+    Help: You're beyond it.
+
+Contributing
+
+Oh, you want to contribute? How quaint. Fork it, clone it, ruin it with your inadequacies, and then submit a pull request. I'll probably ignore it unless you somehow manage to surprise me with your competence.
+License
+
+This code is released under the MIT License. Use it to clutter the web with more subpar logos. As if there weren’t enough already.
+Contact
+
+Don't.
---
+++ index.js
@@ -0,0 +1,63 @@
+const inquirer = require('inquirer')
+const {Triangle, Circle, Square} =  require('./lib/shapes.js')
+const fs = require('fs')
+
+
+// our list of prompts for the user 
+const questions = [
+    {
+        type: 'input',
+        name:'text',
+        message:'!Feed Me 3 Letters!',
+        validate: function(answers){
+            if (answers.length > 3){
+                return console.log('WHY SOME MANY? TO MANY! TRY AGAIN!')
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name:'txtColor',
+        message:'I NEED MORE! HEX COLOR NOW!!',
+    },
+    {
+        type:'list',
+        name:'listShapes',
+        message:'NOW YOU GET SHAPE!',
+        choices:['Circle','Square','Triangle']
+    },
+    {
+        type:'input',
+        name:'shapeColor',
+        message:'NOW SHAPE COLOR!!!!!!!!!!',
+    },
+]
+
+inquirer.prompt(questions).then(answers =>{
+   
+    switch(answers.listShapes){
+     case 'Triangle':
+        return new Triangle(answers.text, answers.txtColor, answers.shapeColor);
+    case 'Circle':
+        return new Circle(answers.text, answers.txtColor, answers.shapeColor);
+    case 'Square':
+        return new Square(answers.text, answers.txtColor, answers.shapeColor);
+    }
+}).then(shap =>{
+    const svg = `
+    <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
+
+    ${shap.render()}
+    ${shap.renderText()}
+    
+    </svg>`;
+
+    fs.writeFile('./examples/logo.svg', svg, err => {
+        if (err) {
+            console.error('git GUD:', err);
+            return;
+        }
+        console.log('SHIT LOGO!!!!');
+    });
+})
